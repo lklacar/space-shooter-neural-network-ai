4235d2d9c9649038c881bc07bdb83afc59614c79
Luka Klacar <l.klacar@vegaitsourcing.rs>
2016-06-03 16:24:53 +0200

    Added multiple body Box2D example

--- core/src/com/lukaklacar/spaceshooter/graphics/screens/GameScreen.java
+++ core/src/com/lukaklacar/spaceshooter/graphics/screens/GameScreen.java
@@ -14,7 +14,41 @@ public class GameScreen extends AbstractBox2dScreen {
     @Override
     public void show() {
         super.show();
+        addBody1();
+        addBody2();
 
+    }
+
+
+    private void addBody2() {
+
+        BodyDef bodyDef = new BodyDef();
+
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+
+        bodyDef.position.set(0, 0);
+
+
+        Body body = getWorld().createBody(bodyDef);
+
+        PolygonShape circle = new PolygonShape();
+        circle.setAsBox(10, 0.1f);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = circle;
+        fixtureDef.density = 0.5f;
+        fixtureDef.friction = 0.4f;
+        fixtureDef.restitution = 0.6f;
+
+
+        Fixture fixture = body.createFixture(fixtureDef);
+
+
+        circle.dispose();
+    }
+
+
+    private void addBody1() {
 
         BodyDef bodyDef = new BodyDef();
 
@@ -26,7 +60,7 @@ public class GameScreen extends AbstractBox2dScreen {
         Body body = getWorld().createBody(bodyDef);
 
         CircleShape circle = new CircleShape();
-        circle.setRadius(0.5f);
+        circle.setRadius(0.3f);
 
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = circle;
@@ -36,6 +70,8 @@ public class GameScreen extends AbstractBox2dScreen {
 
 
         Fixture fixture = body.createFixture(fixtureDef);
+
+        body.applyAngularImpulse(-0.1f, true);
 
 
         circle.dispose();
